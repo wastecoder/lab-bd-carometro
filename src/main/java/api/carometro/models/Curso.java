c0a392a9a44ebe8ce324338cbba34d00748dbf40
Paulino <paulinomendes03@gmail.com>
2024-10-22 07:58:51 -0300

    fix(Curso): adiciona o @Enumerated para o Enum TipoCurso

--- src/main/java/api/carometro/models/Curso.java
+++ src/main/java/api/carometro/models/Curso.java
@@ -17,6 +17,7 @@ public class Curso {
     private String nome;
 
     @Column(name = "tipo", length = 30, nullable = false)
+    @Enumerated(EnumType.STRING)
     private TipoCurso tipo;
 
     @Column(name = "modalidade", length = 10, nullable = false)
