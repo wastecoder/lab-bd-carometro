b76ec5eb6154161d7b50d919663f5052c047d410
Paulino <paulinomendes03@gmail.com>
2024-11-11 10:42:15 -0300

    feat(Curso): adiciona o Bootstrap Modal para confirmar exclusão

--- src/main/resources/static/js/curso.js
+++ src/main/resources/static/js/curso.js
@@ -1,7 +1,4 @@
 function deletarCurso(id, nome) {
-    option = confirm("Deseja excluir o curso [" + nome + "]?");
-    if (option) {
-        alert("Curso excluído com sucesso!");
-        window.location.replace("/cursos/deletar/" + id);
-    }
+    document.getElementById('modalName').innerHTML = nome;
+    document.getElementById('modalConfirm').setAttribute("href", "/cursos/deletar/" + id);
 }
