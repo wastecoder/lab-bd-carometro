fd47395e80b1b5d3c91a29380f95bec8bcc4622a
Paulino <paulinomendes03@gmail.com>
2025-04-11 17:42:15 -0300

    feat(ADM): adiciona um ADM padrão em memória

---
+++ src/main/java/api/carometro/configs/SecurityConfig.java
@@ -0,0 +1,59 @@
+package api.carometro.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User
+                .withUsername("admin@fatec.sp.gov.br")
+                .password(passwordEncoder.encode("123456"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        // TODOS: arquivos estáticos
+                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+
+                        // TODOS: home e perfis
+                        .requestMatchers("/", "/alunos", "/alunos/perfil/**").permitAll()
+
+                        // ADM: todas URLs
+                        .anyRequest().hasRole("ADMIN")
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                );
+
+        return http.build();
+    }
+}
